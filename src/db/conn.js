e17eadf81b5da6f3680de3e9d92ea9b94dd2e04c
KnightNoder <lavakumar@codeastra.com>
2022-01-10 01:53:25 +0530

    process.env check for db with $

--- src/db/conn.js
+++ src/db/conn.js
@@ -3,16 +3,20 @@ const DB = process.env.DB || 'mongodb';
 const DB_HOST = process.env.HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || '27017';
 
-console.log(`${DB}://${DB_HOST}:${DB_PORT}/testApp`);
 mongoose
-  .connect(`${process.env.MONGODB_URL}`, {
-    // .connect(`mongodb://localhost:27017/testApp`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    `mongodb+srv://lava:password_123@freecluster.siwam.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => {
     console.log('connection succesfull');
   })
   .catch((e) => {
     console.log(`No connection due to ${e}`);
   });
+
+// console.log(`${DB}://${DB_HOST}:${DB_PORT}/testApp`);
+// .connect(`mongodb://localhost:27017/testApp`, {
