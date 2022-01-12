010b6a0472b239cb3a1c6957bb6e68ea279296df
KnightNoder <lavakumar@codeastra.com>
2022-01-12 20:25:35 +0530

    error handle error skipped

--- src/helpers/errorHandler.js
+++ src/helpers/errorHandler.js
@@ -1,7 +1,7 @@
 function error(err, req, res, next) {
-  const isLoggedIn = req.session.user ? true : false;
+  // const isLoggedIn = req.session.user ? true : false;
   res.render('404', {
-    isLoggedOut: !isLoggedIn,
+    // isLoggedOut: !isLoggedIn,
     error: 'Internal server error',
   });
 }
